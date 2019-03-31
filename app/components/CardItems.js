0f5faa58d41b6897238a07f0cbe0abb1304e92f7
Pranav Talwar <talwar.pranav123@gmail.com>
2019-03-31 12:47:03 +0800

    .gitignore added

--- app/components/CardItems.js
+++ app/components/CardItems.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Image } from 'react-native';
 import { CardItem, Thumbnail, Text, Left, Body, View, Card } from 'native-base';
 
-const CardItems = ({coursecategory, imgUri}) =>(
+const CardItems = ({ coursecategory, imgUri }) => (
     <View>
         <Card>
             <CardItem>
                 <Left>
-                    <Thumbnail source={{url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiAXkasEdKdzfzK2EXAzS7_N_ygUzAOoMkDN8sxNb_0-8xKzaq'}} />
+                    <Thumbnail source={{ url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiAXkasEdKdzfzK2EXAzS7_N_ygUzAOoMkDN8sxNb_0-8xKzaq' }} />
                     <Body>
                         <Text>{coursecategory}</Text>
                     </Body>
                 </Left>
             </CardItem>
             <CardItem cardBody>
-                <Image source={{url:imgUri}} style={{height: 10, width: null, flex: 1}}/>
+                <Image source={{ url: imgUri }} style={{ height: 10, width: null, flex: 1 }} />
             </CardItem>
         </Card>
     </View>
--- app/components/CoursesCat.js
+++ app/components/CoursesCat.js
@@ -1,17 +1,17 @@
-import React, { Component }from 'react';
-import { Container, Content} from 'native-base';
+import React, { Component } from 'react';
+import { Container, Content } from 'native-base';
 import CardItems from './CardItems';
 
 const CoursesCat = () => (
   <Container>
-        <Content>
-            <CardItems coursecategory={'Law'} imgUri={''}/>
-            <CardItems coursecategory={'Finance'} imgUri={''}/>
-            <CardItems coursecategory={'Science'} imgUri={''}/>
-            <CardItems coursecategory={'Technology'} imgUri={''}/>
-            <CardItems coursecategory={'Language'} imgUri={''}/>
-            <CardItems coursecategory={'Cooking'} imgUri={''}/>
-        </Content>  
+    <Content>
+      <CardItems coursecategory={'Law'} imgUri={''} />
+      <CardItems coursecategory={'Finance'} imgUri={''} />
+      <CardItems coursecategory={'Science'} imgUri={''} />
+      <CardItems coursecategory={'Technology'} imgUri={''} />
+      <CardItems coursecategory={'Language'} imgUri={''} />
+      <CardItems coursecategory={'Cooking'} imgUri={''} />
+    </Content>
   </Container>
 );
 
