23b16f00f56eb8de139fffab3babf202e038d22f
Rajat Jain <jain.rajat50@gmail.com>
2019-04-21 17:26:32 +0800

    Login Screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-
+import { Text, View, TextInput, StyleSheet} from 'react-native';
+import {Container, Content, Header, Form, Input, Item, Label, Button } from 'native-base'
 class Login extends React.Component{
     render()
     {
       return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button title="Login"
-        onPress={()=>this.props.navigation.navigate('Drawer')}
-        />
-    </View>
+          <Container style={styles.container}>
+            <Form>
+              <Item floatingLabel>
+                <Label>Email </Label>
+                <Input autoCorrect={false} autoCapitalize="none" />
+                </Item>
+              <Item floatingLabel style={{marginBottom: 10}}>
+                <Label>Password </Label>
+                <Input secureTextEntry = {true} autoCorrect={false} autoCapitalize="none" />
+                </Item>
+                <Button full rounded styles={ {marginTop: 20,} } onPress={()=>this.props.navigation.navigate('Drawer')}>
+                <Text style={{color:'white'}}> Login </Text>
+                 </Button>
+            </Form>
+            </Container>
       );
     }
   }
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+    },
+  })
 export default Login;
