f1036bda107ac123ecea1dfb5e8c98a10eadcac9
Rajat Jain <jain.rajat50@gmail.com>
2019-04-21 20:38:29 +0800

    Added Login Functionality

--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,52 @@
 import React from 'react';
 import { Text, View, TextInput, StyleSheet} from 'react-native';
 import {Container, Content, Header, Form, Input, Item, Label, Button } from 'native-base'
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_0d1eCp0ajEsK3e9Xw-NT4sE0G3cRFJ4",
+  authDomain: "empoweru-b520c.firebaseapp.com",
+  databaseURL: "https://empoweru-b520c.firebaseio.com",
+  projectId: "empoweru-b520c",
+  storageBucket: "empoweru-b520c.appspot.com",
+  messagingSenderId: "443347823798"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 class Login extends React.Component{
+
+    constructor(props) {
+      super(props)
+      this.state=({
+        email: '',
+        password: ''
+      })
+    }
+      signUpUser =(email, password) => {
+        try {
+          if(this.state.password.length <6) {
+            alert("Please enter at least 6 characters");
+            return;
+          }
+          firebase.auth().createUserWithEmailAndPassword(email,password)
+          this.props.na
+        }
+        catch(error) {
+          console.log(error.toString());
+        }
+
+      }
+      loginUser = (email, password) => {
+        try {
+          firebase.auth().signInWithEmailAndPassword(email, password)
+        }
+        catch(error) {
+          console.log(error.toString());
+        }
+        
+      }
+    
     render()
     {
       return(
@@ -9,14 +54,27 @@ class Login extends React.Component{
             <Form>
               <Item floatingLabel>
                 <Label>Email </Label>
-                <Input autoCorrect={false} autoCapitalize="none" />
+                <Input 
+                autoCorrect={false} 
+                autoCapitalize="none"
+                onChangeText = {(email) => this.setState({email})} />
                 </Item>
+              
               <Item floatingLabel style={{marginBottom: 10}}>
                 <Label>Password </Label>
-                <Input secureTextEntry = {true} autoCorrect={false} autoCapitalize="none" />
+                <Input 
+                  secureTextEntry = {true}
+                  autoCorrect={false} 
+                  autoCapitalize="none" />
+                  onChangeText = {(password)=> this.setState({password})}
                 </Item>
-                <Button full rounded style={ {marginTop: 20,} } onPress={()=>this.props.navigation.navigate('Drawer')}>
+               
+                <Button full rounded style={ {marginTop: 10} } onPress={()=>this.loginUser(this.state.email, this.state.password )}>
                 <Text style={{color:'white'}}> Login </Text>
+                 </Button>
+
+                 <Button full rounded style={ {marginTop: 10,} } onPress={()=>this.signUpUser(this.state.email, this.state.password)}>
+                <Text style={{color:'white'}}> Register </Text>
                  </Button>
             </Form>
             </Container>
